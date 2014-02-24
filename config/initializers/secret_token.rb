9db6c6b7ad5f6e1c9c053655b6b8f9e0def1003e
Nick Isaacs <nisaacs@splickit.com>
2014-02-23 19:09:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BigDataRailsServer::Application.config.secret_key_base = 'cb2a0d864c2d9a3596310efda1799a6d9d028460f3ba70b3ce48b021fe593bd23f8f9a408c4c818489abd7d3c60a1cf60dcc1b318cfa5ca2b5a57309118a20f8'
