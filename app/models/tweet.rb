e685ac2e299b5c0d3ebc51a2ba687ba000711957
Nick Isaacs <nisaacs@splickit.com>
2014-04-10 15:48:35 -0600

    tweet model, include simple elastic search, wire up elasticsearch_route

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,9 +1,11 @@
 class Tweet
+  extend SimpleElasticSearch
 
   ELASTIC_SEARCH_TWEET_TYPE = 'tweet'
 
   def self.all
-
+    results = self.all_docs_in_index $elasticsearch_config[:host], $elasticsearch_config[:port], $elasticsearch_index
+    results.hits.hits
   end
 
   def self.search_str(str)
@@ -14,7 +16,7 @@ class Tweet
             }
         }
     }
-    result = SimpleElasticSearch.search $elasticsearch_config[:host], $elasticsearch_config[:port], index_name, query, ELASTIC_SEARCH_TWEET_TYPE
+    result = self.search $elasticsearch_config[:host], $elasticsearch_config[:port], $elasticsearch_index, query, ELASTIC_SEARCH_TWEET_TYPE
     result.hits.hits
   end
 
