5a37ceaaae6c6e5dd713a70fab7e0e1780d6c591
Ryan Kophs <ryan@K2.(none)>
2014-04-13 21:11:16 -0600

    control layout is complete

--- app/assets/javascripts/source/controllers/dashboard.js
+++ app/assets/javascripts/source/controllers/dashboard.js
@@ -5,11 +5,57 @@ define(['jquery', 'underscore', 'source/modules/controller'], function($, _, con
 
         $scope.data = {
             map: null,
-            pointarray: null,
             heatmap: null,
-            mapOptions: null
+            heatmapOptions: null,
+            mapOptions: null,
         };
-        
+
+        $scope.radius_stop = function(event, ui) {
+            $scope.update_radius(ui.value);
+        }
+
+        $scope.update_radius = function(value) {
+            if (!$scope.data.heatmap) {
+                return;
+            }
+
+            $scope.data.heatmap.set('radius', value);
+        };
+
+        $scope.opacity_stop = function(event, ui) {
+            $scope.update_opacity(ui.value);
+        };
+
+        $scope.update_opacity = function(value) {
+            if (!$scope.data.heatmap) {
+                return;
+            }
+            $scope.data.heatmap.set('opacity', value);
+        };
+
+        $scope.update_map_type = function(type) {
+            var id = google.maps.MapTypeId.SATELLITE;
+            switch (type) {
+                case "road":
+                    id = google.maps.MapTypeId.ROADMAP;
+                    break;
+                case "satellite":
+                    id = google.maps.MapTypeId.SATELLITE;
+                    break;
+                case "hybrid":
+                    id = google.maps.MapTypeId.HYBRID;
+                    break;
+                case "terrain":
+                    id = google.maps.MapTypeId.TERRAIN;
+                    break;
+                default:
+                    id = google.maps.MapTypeId.SATELLITE;
+                    break;
+            }
+            $scope.data.mapOptions.mapTypeId = id;
+            $scope.data.map.setOptions($scope.data.mapOptions);
+        };
+
         $scope.query_params = [{include: true, input: ""}];
 
         $scope.init = function() {
@@ -18,22 +64,26 @@ define(['jquery', 'underscore', 'source/modules/controller'], function($, _, con
                 center: new google.maps.LatLng(39.7392, -104.9847),
                 mapTypeId: google.maps.MapTypeId.SATELLITE
             };
+            $scope.data.heatmapOptions = {
+                radius: 20,
+                opacity: 0.8,
+                dissipating: true
+            };
         };
-        
-        $scope.toggle_input_type = function(index){
-            $scope.query_params[index].include =  !$scope.query_params[index].include;
+
+        $scope.toggle_input_type = function(index) {
+            $scope.query_params[index].include = !$scope.query_params[index].include;
         };
-        
-        $scope.add_query_param = function(){
-            $scope.query_params.push({include:true, input:""});
+
+        $scope.add_query_param = function() {
+            $scope.query_params.push({include: true, input: ""});
         };
-        
-        $scope.remove_query_param = function(index){
+
+        $scope.remove_query_param = function(index) {
             $scope.query_params.splice(index, 1);
-            console.log($scope.query_params);
         };
-        
-        $scope.submit_query = function(){
+
+        $scope.submit_query = function() {
             //submit query goes here.
         };
 
@@ -52,44 +102,33 @@ define(['jquery', 'underscore', 'source/modules/controller'], function($, _, con
             $scope.data.heatmap.setMap($scope.data.heatmap.getMap() ? null : $scope.data.map);
         };
 
-        $scope.changeGradient = function() {
-            if (!$scope.data.heatmap) {
+        $scope.update_heatmap = function(ops) {
+            if (!$scope.data.map) {
                 return;
             }
-
-            var gradient = [
-                'rgba(0, 255, 255, 0)',
-                'rgba(0, 255, 255, 1)',
-                'rgba(0, 191, 255, 1)',
-                'rgba(0, 127, 255, 1)',
-                'rgba(0, 63, 255, 1)',
-                'rgba(0, 0, 255, 1)',
-                'rgba(0, 0, 223, 1)',
-                'rgba(0, 0, 191, 1)',
-                'rgba(0, 0, 159, 1)',
-                'rgba(0, 0, 127, 1)',
-                'rgba(63, 0, 91, 1)',
-                'rgba(127, 0, 63, 1)',
-                'rgba(191, 0, 31, 1)',
-                'rgba(255, 0, 0, 1)'
-            ];
-            $scope.data.heatmap.set('gradient', $scope.data.heatmap.get('gradient') ? null : gradient);
+            if (!$scope.data.heatmap) {
+                $scope.data.heatmap = new google.maps.visualization.HeatmapLayer({
+                    map: $scope.data.map
+                });
+            }
+            $scope.data.heatmap.setValues(ops);
         };
-
-        $scope.changeRadius = function() {
-            if (!$scope.data.heatmap) {
-                return;
+        
+        $scope.update_data = function(data){
+            if($scope.data.heatmapOptions.data){
+                $scope.data.heatmapOptions.data.clear();
             }
-
-            $scope.data.heatmap.set('radius', $scope.data.heatmap.get('radius') === 20 ? 50 : 20);
+            $scope.data.heatmapOptions.data = data;
         };
-
-        $scope.changeOpacity = function() {
-            if (!$scope.data.heatmap) {
-                return;
+        
+        $scope.get_data_interval = function(){
+            if($scope.dataInterval){
+                clearInterval($scope.dataInterval);
+                $scope.dataInterval = null;
+            } else {
+                $scope.dataInterval = setInterval(function(){$scope.get_data()}, 10000); //10 seconds
             }
-
-            $scope.data.heatmap.set('opacity', $scope.data.heatmap.get('opacity') ? null : 0.2);
+            
         };
 
         $scope.get_data = function() {
@@ -103,14 +142,9 @@ define(['jquery', 'underscore', 'source/modules/controller'], function($, _, con
                     for (var i in parsedData) {
                         coordArr[i] = new google.maps.LatLng(parsedData[i][0], parsedData[i][1]);
                     }
-                    
-                    $scope.data.pointArray = new google.maps.MVCArray(coordArr);
-                    $scope.data.heatmap = new google.maps.visualization.HeatmapLayer({
-                        data: $scope.data.pointArray,
-                        radius: 20
-                    });
 
-                    $scope.$emit('update_map');
+                    $scope.update_data(new google.maps.MVCArray(coordArr));
+                    $scope.update_heatmap($scope.data.heatmapOptions);
                 }
             });
         };
--- app/assets/javascripts/source/directives/map.js
+++ app/assets/javascripts/source/directives/map.js
@@ -6,13 +6,9 @@ define(['source/modules/directive'], function(directives) {
             restrict: 'E',
             templateUrl: '/assets/source/templates/map.html',
             link: function(scope) {
-                scope.$onRootScope('update_map', function(event, message) {
-                    scope.data.heatmap.setMap(scope.data.map);
-                    scope.$emit('map_updated');
-                });
-                scope.get_data();
                 scope.data.elem = document.getElementById('map-canvas');
                 scope.setup_new_map();
+                scope.get_data();
             }
         };
     });
